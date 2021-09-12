292de5a098600d7d8e51ac27cac7cac9bac035ad
Guilherme Casali <guilhermecasali.dev@gmail.com>
2021-09-12 20:56:23 -0300

    updated and created homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News about <br /> the <span>React</span> World
+          </h1>
+          <p>
+            Get access to all publication <br />
+            <span>for $9,90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
