cdf80662228eeceb8f3f619362bd3453165bc725
Guilherme Casali <guilhermecasali.dev@gmail.com>
2021-09-13 23:32:38 -0300

    added static site generation to the project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -22,7 +22,7 @@ export default function Home({ product }: HomeProps) {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👋 Hey, welcome</span>
+          <span>Hey, welcome! 👋</span>
           <h1>
             News about <br /> the <span>React</span> World
           </h1>
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JZQVwGuylvm8VaXWwMDee4H");
 
   const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
