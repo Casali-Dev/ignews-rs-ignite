8d90ec6ab624c1cdd856f4c3937c7cdb244e2b75
Guilherme Casali <guilhermecasali.dev@gmail.com>
2021-09-18 23:02:22 -0300

    finished using faunadb module

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,16 +16,32 @@ export default NextAuth({
   ],
   // jwt: {
   //   signingKey: process.env.SIGNIN_KEY,
-  // },
+  // }, read documentation for next auth jwt warning
   callbacks: {
     async signIn(user, account, profile) {
-      const email = user
+      const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
